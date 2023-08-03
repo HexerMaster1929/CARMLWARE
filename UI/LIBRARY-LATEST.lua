455fac405a5a22e13637555e34a3cad00a7a4f88
Mark <94270731+HexerMaster1929@users.noreply.github.com>
2023-08-03 15:24:02 -0700

    Update LIBRARY-LATEST.lua

--- UI/LIBRARY-LATEST.lua
+++ UI/LIBRARY-LATEST.lua
@@ -2282,7 +2282,38 @@ function RayfieldLibrary:CreateWindow(Settings)
 	SettingsTab:CreateButton({
 		Name = "Join Discord",
 		Callback = function()
-			print("Ass")
+			local Request = http_request or syn and syn.request or request or nil
+local CurrentDiscordInvite = isfile('Invite.CARAMEL') and readfile('Invite.CARAMEL') or nil
+if Request then
+    local Invite
+    local r = Request(
+    {
+        ['Method'] = 'GET',
+       --[[ ['Headers'] = {
+            ['discordLinkRequest'] = "true"
+        },]]
+        ['Url'] = 'https://raw.githubusercontent.com/HexerMaster1929/CARMLWARE/main/DISCORD.txt'
+    })
+    if r.StatusCode ~= 200 or not r.Successful then
+        Invite = "7eMaDgQq64"
+    else
+        Invite = r.Body
+    end
+    if not CurrentDiscordInvite or CurrentDiscordInvite ~= Invite then
+        Request(
+            {
+                ['Method'] = 'POST',
+                ['Headers'] = {
+                    ["origin"] = 'https://discord.com',
+                    ["Content-Type"] = "application/json"
+                },
+                ['Url'] = 'http://127.0.0.1:6463/rpc?v=1',
+                ['Body'] = game:GetService('HttpService'):JSONEncode({cmd="INVITE_BROWSER",args={code=Invite},nonce=game:GetService('HttpService'):GenerateGUID(false):lower()})
+            }    
+        )
+        writefile('Invite.CARAMEL',Invite)
+    end
+end
 		end,
 	})
 	
